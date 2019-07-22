43a5554789c6738a889a8a5ece957cb4b317ff60
JakubK <jakubkuniszewski1@gmail.com>
2019-07-22 16:54:05 +0200

    Replace hardcoded path with path native to the Environment OS

--- LinCon.Core/Services/PathProvider.cs
+++ LinCon.Core/Services/PathProvider.cs
@@ -1,9 +1,11 @@
+using System;
+using System.IO;
 using LinCon.Core.Services.Abstract;
 
 namespace LinCon.Core.Services
 {
   public class PathProvider : IPathProvider
   {
-    public string DbFileName => "D:/Database.db";
+    public string DbFileName => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) ,"LinCon/Database.db");
   }
 }
