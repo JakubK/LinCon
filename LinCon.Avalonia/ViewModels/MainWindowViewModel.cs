7d4f985b0a8f824106ace7e5e7a78835c8af3dfa
JakubK <jakubkuniszewski1@gmail.com>
2019-07-23 12:56:22 +0200

    Alter existing MainWindow to be reactive and add ViewModel to it

--- LinCon.Avalonia/ViewModels/MainWindowViewModel.cs
+++ LinCon.Avalonia/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reactive;
 using System.Text;
+using ReactiveUI;
 
 namespace LinCon.Avalonia.ViewModels
 {
-    public class MainWindowViewModel : ViewModelBase
+    public class MainWindowViewModel : ReactiveObject, IScreen
     {
-        public string Greeting => "Hello World!";
+        
+        public RoutingState Router { get; } = new RoutingState();     
+        public ReactiveCommand<Unit, IRoutableViewModel> GoNext { get; }
+        public ReactiveCommand<Unit, Unit> GoBack => Router.NavigateBack;
+
+        public MainWindowViewModel()
+        {
+            GoNext = ReactiveCommand.CreateFromObservable(
+                () => Router.Navigate.Execute(new MenuViewModel(this))
+            );
+
+            GoNext.Execute().Subscribe();
+        }
     }
 }
