8f3addff78050bf1e7b4480e282d45718a2b55e4
JakubK <jakubkuniszewski1@gmail.com>
2019-07-26 12:54:46 +0200

    Inject ICaseRepository and ICaseImporter to CaseImportViewModel and fill the Drop Task method with calls to them

--- LinCon.Avalonia/ViewModels/CaseImportViewModel.cs
+++ LinCon.Avalonia/ViewModels/CaseImportViewModel.cs
@@ -1,7 +1,9 @@
 using System.Reactive;
 using System.Threading.Tasks;
 using Avalonia.Input;
+using LinCon.Core.Services.Abstract;
 using ReactiveUI;
+using Splat;
 
 namespace LinCon.Avalonia.ViewModels
 {
@@ -9,16 +11,23 @@ namespace LinCon.Avalonia.ViewModels
   {
     public IScreen HostScreen { get; }
     public string UrlPathSegment {get;} = System.Guid.NewGuid().ToString().Substring(0,5);
+
+    ICaseRepository _caseRepository;
+    ICaseImporter _caseImporter;
+
     public CaseImportViewModel(IScreen screen)
     { 
       HostScreen = screen;
       DropCommand = ReactiveCommand.CreateFromTask<DragEventArgs>(Drop);
-   
+      
+      _caseRepository = Locator.Current.GetService<ICaseRepository>();
+      _caseImporter = Locator.Current.GetService<ICaseImporter>();
     }
     public ReactiveCommand<DragEventArgs,Unit> DropCommand { get; }
     private Task Drop(DragEventArgs e)
     {
-      
+      var cases = _caseImporter.Import(e.Data.GetFileNames());
+      _caseRepository.Insert(cases);
       return Task.FromResult(0);
     }
   }
