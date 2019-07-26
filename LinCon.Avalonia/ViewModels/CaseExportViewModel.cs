8cbf2f52e95f9e355883a971af2bfb0cd29dd92f
JakubK <jakubkuniszewski1@gmail.com>
2019-07-26 16:24:03 +0200

    Inject ICaseExpoter and implement ExportCasesCommand and Bind it to View

--- LinCon.Avalonia/ViewModels/CaseExportViewModel.cs
+++ LinCon.Avalonia/ViewModels/CaseExportViewModel.cs
@@ -1,13 +1,23 @@
 using System.IO;
 using System.Threading.Tasks;
 using Avalonia.Controls;
+using LinCon.Core.Services.Abstract;
 using ReactiveUI;
+using Splat;
 
 namespace LinCon.Avalonia.ViewModels
 {
   public class CaseExportViewModel : ReactiveObject, IRoutableViewModel
   {
-    public IScreen HostScreen { get; }
+    public CaseExportViewModel(IScreen hostScreen, string urlPathSegment, ReactiveCommand openSaveFileDialogCommand, ReactiveCommand exportCasesCommand) 
+        {
+          this.HostScreen = hostScreen;
+              this.UrlPathSegment = urlPathSegment;
+              this.OpenSaveFileDialogCommand = openSaveFileDialogCommand;
+              this.ExportCasesCommand = exportCasesCommand;
+               
+        }
+            public IScreen HostScreen { get; }
     public string UrlPathSegment {get;} = System.Guid.NewGuid().ToString().Substring(0,5);
 
     string exportPath;
@@ -16,10 +26,16 @@ namespace LinCon.Avalonia.ViewModels
       get => exportPath;
       set => this.RaiseAndSetIfChanged(ref exportPath, value);
     }
+
+    ICaseExporter _caseExporter;
+
     public CaseExportViewModel(IScreen screen)
     {
       HostScreen = screen;
       OpenSaveFileDialogCommand = ReactiveCommand.CreateFromTask(OpenSaveFileDialog);
+      ExportCasesCommand = ReactiveCommand.CreateFromTask(ExportCases);
+
+      _caseExporter = Locator.Current.GetService<ICaseExporter>();
     }
     public ReactiveCommand OpenSaveFileDialogCommand {get;}
     private async Task OpenSaveFileDialog()
@@ -30,6 +46,13 @@ namespace LinCon.Avalonia.ViewModels
       {
         ExportPath = result;
       }      
+    }
+
+    public ReactiveCommand ExportCasesCommand {get;}
+    private Task ExportCases()
+    {
+      _caseExporter.Export(null,ExportPath);
+      return Task.FromResult(0);
     }
   }
 }
