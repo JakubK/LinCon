c8d4b265cab36e5cea236100f66da0687b71c929
JakubK <jakubkuniszewski1@gmail.com>
2019-07-26 17:23:19 +0200

    Create ExportItem model class to represent one Case in a set of CheckBoxes

---
+++ LinCon.Avalonia/Models/ExportItem.cs
@@ -0,0 +1,9 @@
+namespace LinCon.Avalonia.Models
+{
+    public class ExportItem
+    {
+      public int ID {get;set;}
+      public string Name {get;set;}
+      public bool IsChecked {get;set;} = false;
+    }
+}
