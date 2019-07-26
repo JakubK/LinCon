548428e8f7f4193e2fb5f768b144c4cf7b0a009e
JakubK <jakubkuniszewski1@gmail.com>
2019-07-26 15:27:58 +0200

    Implement OpenSaveFileDialog Command and bind it to CaseExportView

--- LinCon.Avalonia/ViewModels/CaseExportViewModel.cs
+++ LinCon.Avalonia/ViewModels/CaseExportViewModel.cs
@@ -1,3 +1,6 @@
+using System.IO;
+using System.Threading.Tasks;
+using Avalonia.Controls;
 using ReactiveUI;
 
 namespace LinCon.Avalonia.ViewModels
@@ -7,6 +10,26 @@ namespace LinCon.Avalonia.ViewModels
     public IScreen HostScreen { get; }
     public string UrlPathSegment {get;} = System.Guid.NewGuid().ToString().Substring(0,5);
 
-    public CaseExportViewModel(IScreen screen) => HostScreen = screen;
+    string exportPath;
+    public string ExportPath
+    {
+      get => exportPath;
+      set => this.RaiseAndSetIfChanged(ref exportPath, value);
+    }
+    public CaseExportViewModel(IScreen screen)
+    {
+      HostScreen = screen;
+      OpenSaveFileDialogCommand = ReactiveCommand.CreateFromTask(OpenSaveFileDialog);
+    }
+    public ReactiveCommand OpenSaveFileDialogCommand {get;}
+    private async Task OpenSaveFileDialog()
+    {
+      SaveFileDialog d = new SaveFileDialog();
+      var result = await d.ShowAsync(new Window());
+      if(result != null)
+      {
+        ExportPath = result;
+      }      
+    }
   }
 }
