1af895b51ae35dbb48bf7b31e4923df1a113ac5d
JakubK <jakubkuniszewski1@gmail.com>
2019-07-27 18:33:48 +0200

    Implement Add Case Command and Bind it to the CaseExplorer View

--- LinCon.Avalonia/ViewModels/CaseExplorerViewModel.cs
+++ LinCon.Avalonia/ViewModels/CaseExplorerViewModel.cs
@@ -4,6 +4,7 @@ using System.Reactive;
 using System.Threading.Tasks;
 using AutoMapper;
 using LinCon.Avalonia.Models;
+using LinCon.Core.Models;
 using LinCon.Core.Services.Abstract;
 using ReactiveUI;
 using Splat;
@@ -12,11 +13,6 @@ namespace LinCon.Avalonia.ViewModels
 {
   public class CaseExplorerViewModel : ReactiveObject, IRoutableViewModel 
   {
-    public CaseExplorerViewModel (IScreen hostScreen, string urlPathSegment) 
-    {
-      this.HostScreen = hostScreen;
-      this.UrlPathSegment = urlPathSegment;
-    }
     public IScreen HostScreen { get; }
     public string UrlPathSegment { get; } = System.Guid.NewGuid ().ToString ().Substring (0, 5);
 
@@ -30,22 +26,34 @@ namespace LinCon.Avalonia.ViewModels
     ICaseRepository _caseRepository;
     IMapper _mapper;
 
-    public CaseExplorerViewModel (IScreen screen) {
+    public CaseExplorerViewModel (IScreen screen) 
+    {
       HostScreen = screen;
+
       _caseRepository = Locator.Current.GetService<ICaseRepository> ();
       _mapper = Locator.Current.GetService<IMapper> ();
 
       Cases = _mapper.Map<ExportItem[]> (_caseRepository.GetAll ());
 
       DeleteCommand = ReactiveCommand.CreateFromTask<int, Unit> (Delete);
+      AddCommand = ReactiveCommand.CreateFromTask(Add);
     }
 
     public ReactiveCommand<int, Unit> DeleteCommand { get; }
     private Task<Unit> Delete (int id) 
     {
       _caseRepository.Delete(id);
-      Cases = Cases.Where(x => x.ID != id);
+      Cases = _mapper.Map<List<ExportItem>> (_caseRepository.GetAll ());
       return Task.FromResult (Unit.Default);
+    }
+
+    public ReactiveCommand AddCommand {get;}
+    private Task Add()
+    {
+      Case c = new Case();
+      _caseRepository.Insert(c);
+      Cases = _mapper.Map<List<ExportItem>> (_caseRepository.GetAll ());
+      return Task.FromResult(0);
     }
   }
 }
