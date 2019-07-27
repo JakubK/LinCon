54b1e1a604c69d46f9fff4953affc283793951d7
JakubK <jakubkuniszewski1@gmail.com>
2019-07-27 14:33:40 +0200

    Add Property for representing set of Cases in CaseExplorerViewModel

--- LinCon.Avalonia/ViewModels/CaseExplorerViewModel.cs
+++ LinCon.Avalonia/ViewModels/CaseExplorerViewModel.cs
@@ -1,4 +1,9 @@
+using System.Collections.Generic;
+using AutoMapper;
+using LinCon.Avalonia.Models;
+using LinCon.Core.Services.Abstract;
 using ReactiveUI;
+using Splat;
 
 namespace LinCon.Avalonia.ViewModels
 {
@@ -6,7 +11,18 @@ namespace LinCon.Avalonia.ViewModels
   {
     public IScreen HostScreen { get; }
     public string UrlPathSegment {get;} = System.Guid.NewGuid().ToString().Substring(0,5);
+    public IEnumerable<ExportItem> Cases { get;set; }
 
-    public CaseExplorerViewModel(IScreen screen) => HostScreen = screen;
+    ICaseRepository _caseRepository;
+    IMapper _mapper;
+
+    public CaseExplorerViewModel(IScreen screen)
+    {
+      HostScreen = screen;
+      _caseRepository = Locator.Current.GetService<ICaseRepository>();
+      _mapper = Locator.Current.GetService<IMapper>();
+
+      Cases = _mapper.Map<ExportItem[]>(_caseRepository.GetAll());
+    }
   }
 }
