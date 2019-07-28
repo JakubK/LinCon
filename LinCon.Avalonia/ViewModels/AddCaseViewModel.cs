99e9ece27e6f51ac422764bebb3bc2fc85c60d5d
JakubK <jakubkuniszewski1@gmail.com>
2019-07-28 20:29:32 +0200

    Implement Return and Add Commands in the AddCaseViewModel and Update the Bindings

--- LinCon.Avalonia/ViewModels/AddCaseViewModel.cs
+++ LinCon.Avalonia/ViewModels/AddCaseViewModel.cs
@@ -12,10 +12,48 @@ namespace LinCon.Avalonia.ViewModels
     public string UrlPathSegment {get; } = System.Guid.NewGuid ().ToString ().Substring (0, 5);
 
     public IScreen HostScreen {get;}
+    CaseExplorerViewModel _parent;
 
-    public AddCaseViewModel(IScreen screen)
+    string name;
+    public string Name 
+    {
+      get => name;
+      set => this.RaiseAndSetIfChanged(ref name, value);
+    }
+
+    ICaseRepository _caseRepository;
+
+    public AddCaseViewModel(IScreen screen, CaseExplorerViewModel parent)
     {
         HostScreen = screen;
+        _parent = parent;
+
+        Name = "New Empty Case";
+
+        ReturnCommand = ReactiveCommand.CreateFromTask(Return);
+        AddCommand = ReactiveCommand.CreateFromTask(Add);
+
+        _caseRepository = Locator.Current.GetService<ICaseRepository>();
     }   
+
+    public ReactiveCommand<Unit,Unit> ReturnCommand {get;}
+    private Task<Unit> Return()
+    {
+      HostScreen.Router.NavigateBack.Execute();
+
+      return Task.FromResult(Unit.Default);
+    }
+
+    public ReactiveCommand AddCommand {get;}
+    private Task<Unit> Add()
+    {
+      _caseRepository.Insert(new Case
+      {
+        Name = Name
+      });
+      ReturnCommand.Execute();
+      _parent.RefreshCommand.Execute();
+      return Task.FromResult(Unit.Default);
+    }
   }
 }
