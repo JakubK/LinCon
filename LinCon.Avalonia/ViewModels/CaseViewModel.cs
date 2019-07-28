35e03c678a0bd314b52829a1e4bc8241715564a8
JakubK <jakubkuniszewski1@gmail.com>
2019-07-28 14:17:06 +0200

    Create OpenLinkCommand inside CaseViewModel

--- LinCon.Avalonia/ViewModels/CaseViewModel.cs
+++ LinCon.Avalonia/ViewModels/CaseViewModel.cs
@@ -1,4 +1,9 @@
+using System.Reactive;
+using System.Threading.Tasks;
+using LinCon.Core.Models;
+using LinCon.Core.Services.Abstract;
 using ReactiveUI;
+using Splat;
 
 namespace LinCon.Avalonia.ViewModels
 {
@@ -8,9 +13,27 @@ namespace LinCon.Avalonia.ViewModels
 
     public IScreen HostScreen {get;set;} 
 
+    public Case Case {get;set;}
+    
+    ICaseRepository _caseRepository;
+    ICaseProcessor _caseProcessor;
     public CaseViewModel(IScreen screen, int id)
     {
         HostScreen = screen;
+        
+        _caseRepository = Locator.Current.GetService<ICaseRepository>();
+        _caseProcessor = Locator.Current.GetService<ICaseProcessor>();
+
+        Case = _caseRepository.GetById(id);
+
+        OpenLinkCommand = ReactiveCommand.CreateFromTask<string,Unit>(OpenLink);
+    }
+
+    public ReactiveCommand<string, Unit> OpenLinkCommand {get;}
+    private Task<Unit> OpenLink(string link)
+    {
+      _caseProcessor.ProcessLink(link);
+      return Task.FromResult(Unit.Default);
     }
   }
 }
