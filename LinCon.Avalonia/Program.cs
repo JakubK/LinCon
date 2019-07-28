a7295879e6d83c1929282fd2558b255b7ef16f28
JakubK <jakubkuniszewski1@gmail.com>
2019-07-28 20:06:58 +0200

    Add Blank DeleteCaseViewModel with it's View and Register them in the DI

--- LinCon.Avalonia/Program.cs
+++ LinCon.Avalonia/Program.cs
@@ -58,6 +58,7 @@ namespace LinCon.Avalonia
             Locator.CurrentMutable.Register(() => new CaseExplorerView(), typeof(IViewFor<CaseExplorerViewModel>));
             Locator.CurrentMutable.Register(() => new CaseView(), typeof(IViewFor<CaseViewModel>));
             Locator.CurrentMutable.Register(() => new AddLinkView(), typeof(IViewFor<AddLinkViewModel>));
+            Locator.CurrentMutable.Register(() => new DeleteCaseView(), typeof(IViewFor<DeleteLinkViewModel>));
             Locator.CurrentMutable.Register(() => new DeleteLinkView(), typeof(IViewFor<DeleteLinkViewModel>));
             Locator.CurrentMutable.Register(() => new DeleteManyLinksView(), typeof(IViewFor<DeleteManyLinksViewModel>));
             Locator.CurrentMutable.Register(() => new DeleteManyCasesView(), typeof(IViewFor<DeleteManyCasesViewModel>));
---
+++ LinCon.Avalonia/ViewModels/DeleteCaseViewModel.cs
@@ -0,0 +1,20 @@
+using System.Linq;
+using System.Reactive;
+using System.Threading.Tasks;
+using LinCon.Core.Models;
+using LinCon.Core.Services.Abstract;
+using ReactiveUI;
+using Splat;
+
+namespace LinCon.Avalonia.ViewModels
+{
+  public class DeleteCaseViewModel : ReactiveObject, IRoutableViewModel
+  {
+    public string UrlPathSegment {get; } = System.Guid.NewGuid ().ToString ().Substring (0, 5);
+    public IScreen HostScreen {get;}
+    public DeleteCaseViewModel(IScreen screen)
+    {
+        HostScreen = screen;
+    }   
+  }
+}
