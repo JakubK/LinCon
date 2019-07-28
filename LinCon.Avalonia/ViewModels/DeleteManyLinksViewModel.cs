78ca75337a160d4e7392b044dd94f249d16c9915
JakubK <jakubkuniszewski1@gmail.com>
2019-07-28 17:08:18 +0200

    Add a List of DeleteLinkItems to DeleteManyLinksViewModel and bind it to the View

--- LinCon.Avalonia/ViewModels/DeleteManyLinksViewModel.cs
+++ LinCon.Avalonia/ViewModels/DeleteManyLinksViewModel.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
 using System.Reactive;
 using System.Threading.Tasks;
+using AutoMapper;
+using LinCon.Avalonia.Models;
 using LinCon.Core.Models;
 using LinCon.Core.Services.Abstract;
 using ReactiveUI;
@@ -12,9 +15,19 @@ namespace LinCon.Avalonia.ViewModels
     public string UrlPathSegment {get; } = System.Guid.NewGuid ().ToString ().Substring (0, 5);
     public IScreen HostScreen {get;}
 
-    public DeleteManyLinksViewModel(IScreen screen)
+    ICaseRepository _caseRepository;
+
+    public List<DeleteLinkItem> DeleteLinkItems {get;set;}
+
+    IMapper _mapper;
+
+    public DeleteManyLinksViewModel(IScreen screen, int caseId)
     {
         HostScreen = screen;
+        _caseRepository = Locator.Current.GetService<ICaseRepository>();
+        _mapper = Locator.Current.GetService<IMapper>();
+        var _case = _caseRepository.GetById(caseId);
+        DeleteLinkItems = _mapper.Map<List<DeleteLinkItem>>(_case.Links);
     }   
   }
 }
