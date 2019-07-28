dc4691d403b605c53c05a1682fffc380d620ae60
JakubK <jakubkuniszewski1@gmail.com>
2019-07-28 20:48:03 +0200

    Implement EditCaseViewModel with Return and Edit Commands

--- LinCon.Avalonia/ViewModels/EditCaseViewModel.cs
+++ LinCon.Avalonia/ViewModels/EditCaseViewModel.cs
@@ -11,11 +11,50 @@ namespace LinCon.Avalonia.ViewModels
   public class EditCaseViewModel : ReactiveObject, IRoutableViewModel
   {
     public string UrlPathSegment {get; } = System.Guid.NewGuid ().ToString ().Substring (0, 5);
-
     public IScreen HostScreen {get;}
-    public EditCaseViewModel(IScreen screen)
+
+    private string name;
+    public string Name
+    {
+      get => name;
+      set => this.RaiseAndSetIfChanged(ref name,value);
+    }
+
+    CaseExplorerViewModel _parent;
+
+    ICaseRepository _caseRepository;
+
+    Case Case;
+    public EditCaseViewModel(IScreen screen, CaseExplorerViewModel parent, int caseId)
     {
         HostScreen = screen;
+        _parent = parent;
+
+        _caseRepository = Locator.Current.GetService<ICaseRepository>();
+        Case = _caseRepository.GetById(caseId);
+        Name = Case.Name;
+
+        EditCommand = ReactiveCommand.CreateFromTask(Edit);
+        ReturnCommand = ReactiveCommand.CreateFromTask(Return);
     }   
+
+    public ReactiveCommand EditCommand {get;}
+    private Task<Unit> Edit()
+    {
+      Case.Name = Name;
+      _caseRepository.Update(Case);
+      
+      ReturnCommand.Execute();
+      _parent.RefreshCommand.Execute();
+      
+      return Task.FromResult(Unit.Default);
+    }
+
+    public ReactiveCommand<Unit,Unit> ReturnCommand {get;}
+    private Task<Unit> Return()
+    {
+      HostScreen.Router.NavigateBack.Execute();
+      return Task.FromResult(Unit.Default);
+    }
   }
 }
