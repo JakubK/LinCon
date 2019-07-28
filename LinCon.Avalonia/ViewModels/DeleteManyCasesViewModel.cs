08688f9572f4325dfa67bbb140e8442edc721ac0
JakubK <jakubkuniszewski1@gmail.com>
2019-07-28 19:39:33 +0200

    Create Blank DeleteManyCasesViewModel with it's View and Register them in the DI

---
+++ LinCon.Avalonia/ViewModels/DeleteManyCasesViewModel.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Threading.Tasks;
+using AutoMapper;
+using LinCon.Avalonia.Models;
+using LinCon.Core.Models;
+using LinCon.Core.Services.Abstract;
+using ReactiveUI;
+using Splat;
+
+namespace LinCon.Avalonia.ViewModels
+{
+  public class DeleteManyCasesViewModel : ReactiveObject, IRoutableViewModel
+  {
+    public string UrlPathSegment {get; } = System.Guid.NewGuid ().ToString ().Substring (0, 5);
+    public IScreen HostScreen {get;}
+
+    public DeleteManyCasesViewModel(IScreen screen, CaseViewModel parentView, int caseId)
+    {
+        HostScreen = screen;
+    }   
+  }
+}
