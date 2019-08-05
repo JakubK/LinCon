993206fabdde8bf11d0e23e555ea855f8252da0b
JakubK <jakubkuniszewski1@gmail.com>
2019-08-05 14:15:18 +0200

    Make sure Case's ID won't be exported to avoid import conflicts

--- LinCon.Core/Models/Case.cs
+++ LinCon.Core/Models/Case.cs
@@ -1,11 +1,13 @@
 using System.Collections.Generic;
 using LiteDB;
+using Newtonsoft.Json;
 
 namespace LinCon.Core.Models
 {
     public class Case
     {
         [BsonId]
+        [JsonIgnore]
         public int ID {get;set;}
         public string Name { get;set; }
         public List<Link> Links {get;set;} = new List<Link>();
