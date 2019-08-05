f1f3c6a08097af869118c27b1fd750d9def0cd1b
JakubK <jakubkuniszewski1@gmail.com>
2019-08-05 17:23:16 +0200

    Relocate the Database to the SpecialFolder created by the Environment

--- LinCon.Core/Services/PathProvider.cs
+++ LinCon.Core/Services/PathProvider.cs
@@ -1,9 +1,19 @@
+using System;
+using System.IO;
 using LinCon.Core.Services.Abstract;
 
 namespace LinCon.Core.Services
 {
   public class PathProvider : IPathProvider
   {
-    public string DbFileName => "Database.db";
+    public string DbFileName  
+    {
+      get
+      {
+          var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),"Database.db");
+          Directory.CreateDirectory(Path.GetDirectoryName(path));
+          return path;
+      }
+    }
   }
 }
